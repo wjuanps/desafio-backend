8e17df432e59a08413fe08d50b59fdda89ad7f7b
Juan Soares <wjuan.ps@gmail.com>
2022-10-15 21:27:53 -0300

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'deputies', to: 'deputies#index'
+  get 'deputies/new', to: 'deputies#new'
+  post 'deputies/create', to: 'deputies#create'
+  get 'deputies/:id/:name', to: 'deputies#show', as: 'deputies/show'
+
+  get 'invoices/charts', to: 'invoices#index'
+
+  root 'deputies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
